4ad64082431714ee0a65dc11fa1731cc7338b5ff
Sandeep Rao <urmsandeep@gmail.com>
2021-05-07 10:23:22 +0530

    Update india_covid_vaccine_availability.py

--- india_covid_vaccine_availability.py
+++ india_covid_vaccine_availability.py
@@ -51,13 +51,16 @@ from datetime import date
 cowin_appt_api = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?"
 url_cowin_vax_by_pin = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?"
 
+# Global
+global total_availability
+
 ##########################################################
 # Look for vaccination centers and availability status for
 # given Pincode and Date
 #########################################################
-def cowin_vax_availability_by_pincode (pincode, vax_date, vaccine, log):
-    found=False
- 
+def cowin_vax_availability_by_pincode (pincode, vax_date, vaccine, display_all, minage, log):
+    total_availability=0
+    found = False;
     ##########################
     # Create the request URL
     ##########################
@@ -70,43 +73,68 @@ def cowin_vax_availability_by_pincode (pincode, vax_date, vaccine, log):
     ##########################
     res = requests.get(req_url)
     if (res.status_code != 200):
-        print("Response: Not OK")
+        print("Response:[%s] Not OK Code:" % (res.status_code))
         return
      
     ##########################
     # Parse JSON Response
     ##########################
+    minage = int(minage)
+    found = False
     center_list = json.loads(res.text)
     for center in center_list['centers']:
         for session in center['sessions']:
             availability = session['available_capacity']
+
+            # Filter on specified min_age
+            if (minage != 0) and ((session['min_age_limit'] != int(minage))):
+                break
+           
+            # Filter on Vaccine type 
+            #print(vaccine_name(vaccine, False), session['vaccine'])
+            if (vaccine is not 'Any') and (vaccine_name(vaccine, False) != session['vaccine']):
+                break
+
             if (availability > 0):
                 found=True;
+                total_availability += availability
                 print("\nPinCode: %s | Center: %-25s | MinAge: %-2s | Availability: %-3s | Vaccine: %s" %
                       (pincode, center['name'], session['min_age_limit'], 
                        session['available_capacity'], session['vaccine']))
             else:
-                print("\nPinCode: %s | Center: %-25s | MinAge: %-2s | Availability: %-3s | Vaccine: %s" %
-                      (pincode, center['name'], session['min_age_limit'], 
-                       session['available_capacity'], session['vaccine']))
+                if (display_all):
+                   print("\nPinCode: %s | Center: %-25s | MinAge: %-2s | Availability: %-3s | Vaccine: %s" %
+                         (pincode, center['name'], session['min_age_limit'], 
+                         session['available_capacity'], session['vaccine']))
 
     ###############################################
     # If we did not find even a single center with
     # availability > 0, we declare.. 
     ###############################################
-    if (found == False):
-        print("\n** No Avaliability at any Center **")
+    if (found == False) and (display_all):
+        print("\nPinCode: %s ** No Avaliability at any Center **\n" % pincode)
+    else:
+        if (found == True):
+            print("\nPinCode: %s Total available: %s" % (pincode, total_availability))
 
 
 
 ################################################################
 # TBD: Iternate over pincode "range" to look for vaccination centers
 ################################################################
-def cowin_vax_availability_iterate_pincode (pincode, vax_date, vaccine, log):
+def cowin_vax_availability_iterate_pincode (pincode, vax_date, vaccine, 
+                                            iterate, display_all, minage, log):
     pin = int(pincode)
-    end_pin = pin + 1
+   
+    # If iterate option is specified, iterate to at least 125 pin codes
+    # from initial given pincode
+    if (iterate):
+        end_pin = pin + 125
+    else: 
+        end_pin = pin + 1
+
     while (pin < end_pin):
-        cowin_vax_availability_by_pincode(str(pin), vax_date, vaccine, log)
+        cowin_vax_availability_by_pincode(str(pin), vax_date, vaccine, display_all, minage, log)
         pin += 1
 
 
@@ -139,8 +167,14 @@ def initialize():
                         help="Pin Code")
     parser.add_argument('-d', action="store", dest="vax_date", required=False,
                         help="Date for Vaccine Availability in dd-mm-yyyy format, default=today's date")
-    parser.add_argument('-v', action="store", dest="vaccine", required=False,
+    parser.add_argument('-v', action="store", dest="vaccine", default="Any", required=False,
                         help="Vaccine Type [CX=Covaxin, default=COVISHIELD")
+    parser.add_argument('-a', action="store", dest="minage", default=0, required=False,
+                        help="Vaccine Type [CX=Covaxin, default=COVISHIELD")
+    parser.add_argument('-f', action='store_true', dest="display_all", required=False,
+                        help="Iterate to around 125 pin codes. Uses specified pincode as starting code")
+    parser.add_argument('-i', '--iterate', action='store_true', dest="iterate", required=False,
+                        help="Iterate to around 125 pin codes. Uses specified pincode as starting code")
     parser.add_argument('-l', '--simulate', action='store_true', dest="log", required=False,
                         help="Enable debug logs")
     args = parser.parse_args()
@@ -151,21 +185,22 @@ def initialize():
     else:
         vax_date=args.vax_date
 
+    vaccine = args.vaccine
     ##########################################################
     # Here we just look for any Vaccine availability as the 
     # data on vaccine type is not accurate in CoWin
     ##########################################################
-    vaccine = "Any"
     print("\n======================================================================================")
-    print("Checking availability for Pincode=%s Date=%s Vaccine=%s" %
-          (args.pincode, vax_date, vaccine))
+    print("Checking availability for Pincode=%s Date=%s Age=%s+ Vaccine=%s" %
+          (args.pincode, vax_date, args.minage, vaccine))
     print("========================================================================================")
 
     ###############################
     # Begin look up process...
     ###############################
-    cowin_vax_availability_iterate_pincode (args.pincode, vax_date, vaccine, args.log)
-
+    total_availability = 0
+    cowin_vax_availability_iterate_pincode(args.pincode, vax_date, vaccine, 
+          args.iterate, args.display_all, args.minage, args.log)
 
 if __name__ == "__main__":
     start=time.time()
